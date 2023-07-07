ee7fec5638343584486285e1af4c69f3b4f20976
Abhishek N N <7abhisheknn@gmail.com>
2023-07-07 10:21:22 +0530

    client initiated

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'; 
 
 import { AppComponent } from './app.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { EmployeeFormComponent } from './employees/employee-form/employee-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeesComponent,
-    EmployeeFormComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
